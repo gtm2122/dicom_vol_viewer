55ae452fc1a884f9771f26616dbbbf04b6eda4ba
Gabriel Maliakal <gam2018@med.cornell.edu>
2018-10-26 16:48:49 -0400

    added a study viewer and a vol viewer

--- radiob.py
+++ radiob.py
@@ -6,7 +6,7 @@ from load_scan_dir import *
 from PIL import ImageTk as itk
 import PIL
 import pickle
-
+import scipy.misc
 from decomp_dcm import *
 # root = tk.Tk()
 
@@ -25,27 +25,33 @@ from decomp_dcm import *
 
 
 # vol = []
-
+import scipy.ndimage
 import tkinter.ttk as ttk
 import scipy.misc
+import subprocess
+import png
+from PIL import Image
+#import pypng as png
 class GUI():
     def __init__(self,file_path = '/data/gabriel/TAVR/TAVR_Sample_Study' ):
-    
+        self.study = []
         self.file_path = file_path
         self.vol = []
-        self.val_to_tuple = {}
+        self.val_to_tuple = {} 
         self.counter = -1
-        
+        self.study_to_text = {} ### lookup table linking study number with "button text"
         #self.df  =
-        self.group_df = return_grouped(group_scans_df(self.file_path))
         self.df = group_scans_df(self.file_path)
+        self.group_df = return_grouped(self.df)
         
+        self.vol_name = None
         
         self.root = tk.Tk()
         
         self.v = tk.IntVar()
         self.v.set(0)
-
+        self.study_id = 0
+        self.vol_id = 0
         self.next_var = tk.IntVar()
         self.prev_var = tk.IntVar()
 
@@ -53,9 +59,9 @@ class GUI():
         self.prev_var.set(0)
 
         # get all vols:
-        self.welcome_logo = itk.PhotoImage(PIL.Image.open('welcome.png'))
+        #self.welcome_logo = itk.PhotoImage(PIL.Image.open('welcome.png'))
         self.label  = tk.Label(master=self.root,image=itk.PhotoImage(PIL.Image.open('welcome.png')))    
-        self.label.pack(side='left')
+        self.label.pack(side='bottom')
         
         
         
@@ -63,56 +69,85 @@ class GUI():
         self.b2 = tk.Radiobutton(master=self.root,text='Prev',indicatoron=False,variable=self.prev_var,command=self.prev_b)
         self.b1.pack()
         self.b2.pack()
-        self.header = ['Volume Index','Series No.','Acquisition No.','Phase %','No. of Images','Manufacturer']
+        self.header = ['Volume Index','Series Description','Acquisition No.','Phase %','No. of Images']
         
-        self.tree = ttk.Treeview(master=self.root,columns = self.header,show='headings')
-        self.tree.pack()
+        self.tree_stud = ttk.Treeview(master=self.root,columns = ['Study'],show='headings')
+        self.tree_stud.pack(side='left')
+        
+        self.tree_stud.heading('Study',text='Study',anchor=tk.CENTER)
+        self.tree_stud.column(str(0),stretch=tk.YES)
+        
+        self.tree_vol = ttk.Treeview(master=self.root,columns = self.header,show='headings') 
+        self.tree_vol.pack(side='left')
         
         
         for idx,h in enumerate(self.header):
-            self.tree.heading(self.header[idx],text=self.header[idx],anchor=tk.CENTER)
-            self.tree.column(str(idx),stretch=tk.YES)
+            self.tree_vol.heading(self.header[idx],text=self.header[idx],anchor=tk.CENTER)
+            self.tree_vol.column(str(idx),stretch=tk.YES)
             
         all_keys =list( self.group_df.groups.keys())
         self.w = tk.Message(master=self.root,text = "")
+        
         for i,j in enumerate(all_keys):
             vol_idx = i
-
-            ser_num = j[0]
-            acq_num = j[1]
+#             print('keys')
+#             print(j)
+            ser_num = j[1]
+            acq_num = j[0]
             ph_num  = j[2]
-            man_name = j[3]
-            vol=return_volume(self.df,self.group_df,ser_num,acq_num,ph_num,man_name)
+            man_name =j[3]
+            #ser_des = j[-1]
+            
+            ### READ - "i" is the study id while vol_id is the study's volume id
+            
+            grouped_subset = self.group_df.groups[(acq_num, ser_num, str(ph_num), man_name)] # gives the indices of the grouping
+            group = self.df.loc[grouped_subset]
+            
+            ser_des = group['SeriesDescription']
+            
+            
+            
+            
+            #vol=return_volume(self.df,self.group_df,acq_num,ser_num,ph_num,man_name)
             #print(vol)
-            num_vols = len(vol)
-            num_images = [len(v) for v in vol]
-
+             
+            #print(v)
+            #print(i)
+            self.tree_stud.insert("","end",value=str(vol_idx+1))
+            self.tree_stud.bind("<Double-1>",self.load_volume)
+            
+#             num_vols = len(vol)
+#             num_images = [len(v) for v in vol]
+            
+            
+            
             #num_images.append(len(vol[-1]))
-            button_text = (ser_num,acq_num,ph_num,num_images,man_name)
+#              button_text = (ser_num,acq_num,ph_num,num_images)
+            button_text = (ser_num,acq_num,ph_num,ser_des,man_name)
             
+            self.study_to_text[1+i] = button_text
             
+#             for vol_id in range(0,num_vols):
+#                 #self.v.set(0)
+#                 #print(man_name[0])
+#                 vol_id1 = vol_id +  i
             
-            for vol_id in range(0,num_vols):
-                #self.v.set(0)
-                #print(man_name[0])
-                vol_id1 = vol_id +  i
-            
-                button_text1 = (vol_id1,ser_num,acq_num,ph_num,num_images[vol_id],man_name)
-
-                #print(button_text1)
+#                 button_text1 = (vol_id1,ser_des.values[0],acq_num,ph_num,num_images[vol_id]) 
                 
-                self.tree.insert("","end",values = button_text1)
-                self.tree.bind("<Double-1>",self.load_volume)
+#                 #print(button_text1)
                 
-#                 tk.Radiobutton(master=self.root,\
-#                                text=button_text1
-#                                ,padx=20,\
-#                                variable=self.v,\
-#                                command=self.load_volume,\
-#                                value=i,\
-#                                indicatoron=False).pack(anchor=tk.W)
+#                 self.tree_vol.insert("","end",values = button_text1)
+#                 self.tree_vol.bind("<Double-1>",self.load_volume)
+                
+# #                 tk.Radiobutton(master=self.root,\
+# #                                text=button_text1
+# #                                ,padx=20,\
+# #                                variable=self.v,\
+# #                                command=self.load_volume,\
+# #                                value=i,\
+# #                                indicatoron=False).pack(anchor=tk.W)
 
-                self.val_to_tuple[vol_id1]=button_text1
+#                 self.val_to_tuple[vol_id1]=[button_text1,man_name,ser_num]
                 
                 
         self.annotate_button = tk.Radiobutton(master=self.root,\
@@ -124,28 +159,205 @@ class GUI():
                                         ).pack(anchor=tk.SW)
 
         self.root.mainloop()
+    
+    def resample(self, scan, image, new_spacing=[0.625,0.625,0.625]):
+        # Determine current pixel spacing
+        #spacing = map(float, ([scan[0].SliceThickness] + scan[0].PixelSpacing))
+        spacing = np.array([scan[0].SliceThickness, scan[0].PixelSpacing[0], scan[0].PixelSpacing[1]], dtype=np.float32)
+        #spacing = np.array(list(spacing))
         
+        
+        
+        #print('image dtype')
+        #print(image.dtype)
+        #print(spacing)
+        #spacing = np.array([scan[0].SliceThickness] + scan[0].PixelSpacing, dtype=np.float32)
+        #image = np.array([i.pixel_array for i in scan]).transpose(1,2,0)
+        resize_factor = spacing / new_spacing
+        new_real_shape = image.shape * resize_factor
+        new_shape = np.round(new_real_shape)
+        real_resize_factor = new_shape / image.shape
+        new_spacing = spacing / real_resize_factor
+
+        image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, order = 1, mode='nearest')
+
+        return image, new_spacing   
+    
+    def get_pixels_hu(self,slices):
+    
+        image = np.stack([s.pixel_array for s in slices])
+        # Convert to int16 (from sometimes int16), 
+        # should be possible as values should always be low enough (<32k)
+        image = image.astype(np.int16)
+
+        # Set outside-of-scan pixels to 0
+        # The intercept is usually -1024, so air is approximately 0
+        #image[image == -2000] = 0
+
+        # Convert to Hounsfield units (HU)
+        for slice_number in range(len(slices)):
+
+            intercept = slices[slice_number].RescaleIntercept
+            slope = slices[slice_number].RescaleSlope
+            padding =  slices[slice_number].PixelPaddingValue
+
+            img = image[slice_number]
+            img[img==padding] = 0        
+            # Shift 2 bits based difference 16 -> 14-bit as returned by jpeg2k_bit_depth
+            #padded_pixels = np.where( img & (1 << 14))
+            #image[slice_number] = np.right_shift( image[slice_number], 2)
+
+            if slope != 1:
+                image[slice_number] = slope * image[slice_number].astype(np.float64)
+                image[slice_number] = image[slice_number].astype(np.int16)
+
+            image[slice_number] += np.int16(intercept)
+            #img[padded_pixels] = intercept
+
+        return np.array(image, dtype=np.int16)
+    
     def annotate_func(self):
-        print("Not implemented")
+        ### make the volume folder
+        ### make one folder with png images that are isotropic, another with label inputs (later), add an XML file 
+        ### invoke test.exe
+        
+#         if os.path.isdir('volume')
+#         os.system('')
+        #if self.vol_name is None:
+        
+        
+        #image = np.array([decomp(i[0]) for i in self.vol])
+        
+        if self.vol_name is None:
+            
+            self.w.configure(text = "please select a volume")
+        
+        else:
+            
+            if not os.path.isdir(self.vol_name + '/images'):
+                os.makedirs(str(self.vol_name)+ '/images')
+                os.makedirs(str(self.vol_name)+ '/Labels')
+            ##print(self.vol[0][0])
+            volume_meta = [pydicom.dcmread(v[0]) for v in self.vol]
+            
+            slices=volume_meta
+            ###self.vol is the clicked volume regardless of study
+            images = self.get_pixels_hu(slices)
+            images = images + 1024
+            decomp_img,spacing = self.resample(scan=volume_meta,image=images)
+            spacing = np.round(spacing,3)
+            num_rows = np.round(decomp_img.shape[0],3)
+            num_cols = np.round(decomp_img.shape[1],3)
+            num_z = np.round(decomp_img.shape[2],3)
+            
+#             print(num_rows)
+#             print(num_cols)
+#             print(num_z)
+#             print(decomp_img.shape)
+            
+            for i in range(0,len(decomp_img)):
+                arr = decomp_img[i,:,:]
+                if arr.shape[0]>512:
+                    arr = scipy.misc.resize(arr,(512,512))
+#                 print(arr.min())
+#                 print(arr.max())
+                #np.save('image',arr)
+                array_buffer = arr.tobytes()
+                img = Image.new("I", arr.T.shape)
+                img.frombytes(array_buffer, 'raw', "I;16")
+                img.save(str(self.vol_name)+'/images/image - '+str(i)+'.png')
+
+#             for i,img in enumerate(decomp_img):
+#                 print(img.shape)
+#                 print(type(img))
+#                 print(img.dtype)
+#                 #png.from_array(img, 'L').save('volume'+str(self.vol_name)+'/Images/Image_'+str(i)+'.png')
+# #                 with open('img - '+str(i)+'.png', 'wb') as f:
+# #                     writer = png.Writer(width=img.shape[1], height=img.shape[0], bitdepth=16)
+# #                     z2list = img.reshape(-1, img.shape[1]*img.shape[2]).tolist()
+# #                     writer.write(f, z2list)
+#                 scipy.misc.imsave('volume'+str(self.vol_name)+'/Images/Image_'+str(i)+'.png',img)
+            
+            with open('volume'+str(self.vol_name)+'/volume.xml','w') as f:
+                f.write('<volume>\n')
+                f.write('<Spacing><x>'+str(spacing[2])+'</x><y>'+str(spacing[1])+'</y><z>'+str(spacing[0])+'</z></Spacing>')
+                f.write('\n<Size><x>'+str(num_z)+'</x><y>'+str(num_cols)+'</y><z>'+str(num_rows)+'</z></Size>')
+                f.write('\n</volume>')
+                
+            #print(self.vol_name)
+            #print('D:/Anaconda3/project/TAVR/annotation/text.exe --path volume'+str(self.vol_name))
+            #print('anno_new/text.exe --path volume'+str(self.vol_name))
+            #print(self.vol_name)
+            #print(type(self.vol_name))
+            subprocess.run('anno_new/test.exe --path volume'+str(self.vol_name))
+        
+        ##print("Not implemented")
         
     def load_volume(self,event):
         ### for treeview
+        ### TODO create another tree view with "study" , upon clicking reveals all the "vols" in that study
+        ### implement study level clustering
         
         
+        self.tree_vol.delete(*self.tree_vol.get_children())
+        
+        #button_text = (ser_num,acq_num,ph_num,ser_des,man_name)
+        #return_volume(self.df,self.group_df,acq_num,ser_num,ph_num,man_name)
+        #print(self.tree_stud.selection())
+        #self.vol_name = ''
         self.counter=-1
-        req_tuple = self.val_to_tuple[int(self.tree.selection()[0][1:],16)-1]
-        self.vol = return_volume(self.df,self.group_df,req_tuple[1],req_tuple[2],req_tuple[3],req_tuple[5])
+        req_tuple = self.study_to_text[int(self.tree_stud.selection()[0][1:],16)]
+        #print(req_tuple)
+        #req_tuple = self.val_to_tuple[int(self.tree_stud.selection()[0][1:],16)-1]
+        ##print(req_tuple)
+        
+#         for i in req_tuple:
+#             print(type(i))
+#             print(i)
+        
+        self.study = return_volume(self.df,self.group_df,req_tuple[1],req_tuple[0],req_tuple[2],req_tuple[-1])
         #print(len(vol))
         
-        self.vol = self.vol[req_tuple[0]%len(self.vol)]
-        #self.v.set(0)
+        num_vols = len(self.study)
+        num_imgs = [len(i) for i in self.study]
+        
+        #self.tree_vol.insert()
+        self.header = ['Volume Index','Series Description','Acquisition No.','Phase %','No. of Images']
+        
+        
+        for i in range(0,num_vols):
+            #print(str(i))
+#             print(req_tuple[-2][0])
+#             print(req_tuple[1][0])
+#             print(req_tuple[2][0])
+            #print(req_typle[-2])
+
+            button_text1 = (str(i),req_tuple[-2].values[0],req_tuple[1],req_tuple[2],str(num_imgs[i]))
+            self.tree_vol.insert("","end",values = button_text1)
+            self.tree_vol.bind("<Double-1>",self.load_each_volume)
+        
+#         self.vol = self.vol[req_tuple[0][0]%len(self.vol)]
+#         self.vol_name = req_tuple[0][0]
+# #         print(len(self.vol))
+# #         print(self.vol)
+#         #self.v.set(0)
+#         self.next_b()
+        self.v.set(0)
+#         self.w.configure(text = "viewing  Vol Idx %s, Series %s, Acq Num %s"%(req_tuple[0][0],req_tuple[2],req_tuple[0][3]))
+#         self.w.pack()
+        self.study_id = int(self.tree_stud.selection()[0][1:],16)
+        #self.vol_name = 'Study'+str(int(self.tree_stud.selection()[0][1:],16))
+    def load_each_volume(self,event):
+        
+        vol_idx = (int(self.tree_vol.selection()[0][1:],16)-1) % len(self.study)
+        print(int(self.tree_vol.selection()[0][1:],16)-1)
+        print(vol_idx)
+        self.vol_name = 'study_'+str(self.study_id)+'_vol_'+str(vol_idx)
+        self.vol = self.study[vol_idx]
+        
         self.next_b()
         
-        self.w.configure(text = "viewing  Vol Idx %s, Series %s, Acq Num %s"%(req_tuple[1],req_tuple[2],req_tuple[3]))
-        self.w.pack()
-        
-        
-        
+        print('aa')
     def next_b(self):
         #self.w.destroy()
         
@@ -156,7 +368,8 @@ class GUI():
             self.counter=0
 
         np_img = decomp(self.vol[self.counter][0])
-         
+        if np_img.shape[0]>512:
+            np_img = scipy.misc.imresize(np_img,(512,512))
         scipy.misc.imsave('temp1.jpg',np_img)
         self.next_var.set(0)
         photo= itk.PhotoImage( PIL.Image.open('temp1.jpg'))
